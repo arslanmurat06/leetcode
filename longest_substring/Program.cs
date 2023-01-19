2ce7c985af5549f6f6e00d9641a376a1e28e5e8c
arslanmurat06 <mrtarsln06@gmail.com>
2023-01-19 15:20:22 +0300

    longest substring

---
+++ longest_substring/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+Console.WriteLine(LengthOfLongestSubstring("abcabasdasdas"));
+
+int LengthOfLongestSubstring(string s)
+{
+    int start = 0;
+    int end = 0;
+    int max = 0;
+    HashSet<char> set = new HashSet<char>();
+
+    while (start < s.Length)
+    {
+        char c = s[start];
+
+        while (set.Contains(c))
+        {
+            set.Remove(s[end]);
+            ++end;
+        }
+
+        set.Add(c);
+        max = Math.Max(max, start - end + 1);
+        ++start;
+    }
+
+    return max;
+}
