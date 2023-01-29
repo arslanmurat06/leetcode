d665d62ba4a7d74107cbbc045bc81ab8b98bef29
arslanmurat06 <mrtarsln06@gmail.com>
2023-01-29 18:15:13 +0300

    second way added

--- middle_linked_list/Program.cs
+++ middle_linked_list/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 var node = CreateListNode(new ListNode(), new int[] { 1, 2, 3, 4, 5 });
-MiddleNode(node);
+MiddleNode2(node);
 
 ListNode MiddleNode(ListNode head)
 {
@@ -20,6 +20,27 @@ ListNode MiddleNode(ListNode head)
     nums.RemoveRange(0, middle);
     var result = CreateListNode(newLinked, nums.ToArray());
     return result;
+}
+
+
+//second way
+ListNode MiddleNode2(ListNode head)
+{
+    List<ListNode> nodes = new List<ListNode>();
+    int count = 0;
+    var node = head;
+
+    while (node != null)
+    {
+        nodes.Add(node);
+        node = node.next;
+    }
+
+    var middle = nodes.Count / 2;
+
+    var result = nodes[middle];
+    return result;
+
 }
 
 
